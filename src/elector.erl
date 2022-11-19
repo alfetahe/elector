517fda973f2fd03e8a027cc9c67df7b02e1a2515
anuar <anuar.alfetahe@adm.ee>
2022-11-19 04:11:07 +0200

    Elector callbacks

--- src/elector.erl
+++ src/elector.erl
@@ -1,15 +1,35 @@
 -module(elector).
 -behaviour(gen_server).
 -export([start/0]).
--export([init/1, handle_call/3, handle_cast/2]).
-
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, handle_continue/2]).
 
 start() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
+schedule_election(Time) ->
+    erlang:send_after(Time, ?MODULE, election_schedule).
+
 init(_) ->
-    {ok, #{}}.
-        
+    {ok, #{}, {continue, setup}}.
+
+handle_continue(setup, State) ->
+    net_kernel:monitor_nodes(true),
+    schedule_election(5000),
+
+    {noreply, State}.
+
+handle_info(election_schedule, State) ->
+    {noreply, State};
+handle_info({nodeup, _Node}, State) ->
+    schedule_election(0),
+    {noreply, State};
+handle_info({nodedown, _Node}, State) ->
+    schedule_election(0),
+    {noreply, State};
+handle_info(Msg, State) ->
+    logger:notice("Unexpected message received at elector: " ++ erlang:display(Msg)),
+    {noreply, State}.
+
 handle_call(Msg, _From, State) ->
     {ok, Msg, State}.
 
