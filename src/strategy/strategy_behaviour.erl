62538b9aec3cc967be787188f635ca7c1295df60
Anuar Alfetahe <34630876+alfetahe@users.noreply.github.com>
2022-11-24 09:58:09 +0200

    Update strategy_behaviour.erl

--- src/strategy/strategy_behaviour.erl
+++ src/strategy/strategy_behaviour.erl
@@ -3,3 +3,7 @@
 -type leader() :: node().
 
 -callback elect() -> Leader :: leader().
+
+elect() ->
+	Strategy_module = application.get_env(elector, strategy_module, runtime_strategy),
+	erlang:apply(Strategy_module, elect, []).
