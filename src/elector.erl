a183562825a043143065b87cc2336be1101fbf8d
Anuar Alfetahe <34630876+alfetahe@users.noreply.github.com>
2022-12-02 09:23:10 +0200

    Update elector.erl

--- src/elector.erl
+++ src/elector.erl
@@ -3,3 +3,10 @@
 is_leader?() ->
 	LeaderNode = gen_server:call(election_worker, get_leader),
 	LeaderNode =.= node().
+
+elect() ->
+	gen_server:cast(election_worker, elect_async),
+	ok.
+
+elect_sync() ->
+	gen_server:call(election_worker, elect_sync).
