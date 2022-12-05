a2ef4de511280b01505744072cb699a4758a5990
Anuar Alfetahe <34630876+alfetahe@users.noreply.github.com>
2022-12-05 21:15:30 +0200

    Update runtime_strategy_SUITE.erl

--- test/runtime_strategy_SUITE.erl
+++ test/runtime_strategy_SUITE.erl
@@ -6,5 +6,6 @@ host_node_runtime_test(_Config) ->
 	assert?(is_integer(Resp) and CurrRuntime =< Resp).
 
 elect_test(_Config) ->
+	application:set_env(elector, strategy_module, runtime_strategy),
 	SelectedNode = runtime_strategy:elect(),
 	assert?(SelectedNode =:= node()).
