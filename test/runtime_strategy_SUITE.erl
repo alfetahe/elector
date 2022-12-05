fc635138090dccbce9991eb4e3806b22d1a2b0a7
Anuar Alfetahe <34630876+alfetahe@users.noreply.github.com>
2022-12-05 21:11:58 +0200

    Update runtime_strategy_SUITE.erl

--- test/runtime_strategy_SUITE.erl
+++ test/runtime_strategy_SUITE.erl
@@ -1,6 +1,9 @@
 -module(runtime_strategy_SUITE).
 
 host_node_runtime_test(_Config) ->
-	CurrRuntime = 
-	Resp = host_node_runtime(),
-	assert?(is_integer(Resp) and CurrRuntime < Resp).
+	{_, CurrRuntime} = eroang:statistics(runtime),
+	Resp = runtime_startegy:host_node_runtime(),
+	assert?(is_integer(Resp) and CurrRuntime =< Resp).
+
+elect_test(_Config) ->
+	runtime_strategy:elect(),
