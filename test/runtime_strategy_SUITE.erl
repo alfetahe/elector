8213fd979cbe682a4aba824c29efa376f1ae7032
Anuar Alfetahe <34630876+alfetahe@users.noreply.github.com>
2022-12-06 09:26:06 +0200

    Update runtime_strategy_SUITE.erl

--- test/runtime_strategy_SUITE.erl
+++ test/runtime_strategy_SUITE.erl
@@ -1,7 +1,30 @@
 -module(runtime_strategy_SUITE).
+-include_lib("common_test/include/ct.hrl").
+-compile(export_all).
+
+groups() ->
+	[{runtime_strategy_group, [], [elect_test, host_node_runtime_test}].
+
+all() ->
+	[{group, runtime_strategy_group)}]
+
+suite() ->
+	{_, CurrRuntime} = erlang:statistics(runtime),
+	[{curr_runtime, CurrRuntime}].
+
+init_per_group(_GroupName, _Config) ->
+	ok.
+
+end_per_group(_GroupName, _Config) ->
+	ok.
+
+init_per_suite(_Config) ->
+	ok.
+
+end_per_suite(_Config) ->
+	ok.
 
 host_node_runtime_test(_Config) ->
-	{_, CurrRuntime} = eroang:statistics(runtime),
 	Resp = runtime_startegy:host_node_runtime(),
 	assert?(is_integer(Resp) and CurrRuntime =< Resp).
 
