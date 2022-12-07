d3e3047f5a05fc10869dcf81fa1fc12c852a1328
Anuar Alfetahe <34630876+alfetahe@users.noreply.github.com>
2022-12-07 08:55:33 +0200

    Update config_handler_SUITE.erl

--- test/config_handler_SUITE.erl
+++ test/config_handler_SUITE.erl
@@ -1,1 +1,17 @@
 -module(config_handler_SUITE).
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+-compile(export_all).
+
+groups() ->
+	[{config_handler_group, [], [testidsiin}].
+
+all() ->
+	[{group, config_handler_group)}]
+
+init_per_group(_GroupName, _Config) ->
+	application:set_env(elector, strategy_module, runtime_strategy),
+	ok.
+
+end_per_group(_GroupName, _Config) ->
+	ok.
