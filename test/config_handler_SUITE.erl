de788360c126b83572c22439b907d68a32e74b22
Anuar Alfetahe <34630876+alfetahe@users.noreply.github.com>
2022-12-07 09:08:50 +0200

    Update config_handler_SUITE.erl

--- test/config_handler_SUITE.erl
+++ test/config_handler_SUITE.erl
@@ -9,9 +9,33 @@ groups() ->
 all() ->
 	[{group, config_handler_group)}]
 
-init_per_group(_GroupName, _Config) ->
-	application:set_env(elector, strategy_module, runtime_strategy),
-	ok.
-
 end_per_group(_GroupName, _Config) ->
-	ok.
+	application_set(elector, election_delay, 3000),
+	application_set(elector, strategy_module, runtime_strategy),
+	application_set(elector, sync_start, false),
+	application_set(elector, pre_election_hooks, []),
+	application_set(elector, post_election_hooks, []).
+
+election_delay_test() ->
+    application:set_env(elector, election_delay, 5000),
+	assert?(config_handler:election_delay() =:= 5000).
+
+strategy_module() ->
+    application:set_env(elector, strategy_module, test_module),
+	assert?(config_handler:strategy_module() =:= test_module).
+
+sync_start() ->
+    application:set_env(elector, sync_start, true),
+	assert?(config_handler:sync_start() =:= true).
+
+pre_election_hooks() ->
+		application:set_env(elector, pre_election_hooks, test_hooks()),
+		assert?(config_handler:pre_election_hooks() =:= test_hooks().
+
+post_election_hooks() ->
+		application:set_env(elector, post_election_hooks, test_hooks()),
+	assert?(config_handler:post_election_hooks() =:= test_hooks()).
+
+test_hooks() ->
+	[{test, test, []}].
+	
