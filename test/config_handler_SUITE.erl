9287d5707d87a47755a407ee1c7c73c82dc3dd13
Anuar Alfetahe <34630876+alfetahe@users.noreply.github.com>
2022-12-29 21:06:22 +0200

    Update config_handler_SUITE.erl

--- test/config_handler_SUITE.erl
+++ test/config_handler_SUITE.erl
@@ -1,12 +1,12 @@
 -module(config_handler_SUITE).
 -export([groups/0, all/0, init_per_group/2, end_per_group/2]).
 -export([test_election_delay/1, test_strategy_module/1, test_sync_start/1,
-test_pre_election_hooks_data/1, test_post_election_hooks_data/1, test_hook_trigger/1]).
+test_pre_election_hooks/1, test_post_election_hooks/1, test_hook_trigger/1]).
 
 groups() ->
 	[{config_handler_group, [], [test_election_delay,
-	test_strategy_module, test_sync_start, test_pre_election_hooks_data,
-	test_post_election_hooks_data]}].
+	test_strategy_module, test_sync_start, test_pre_election_hooks,
+	test_post_election_hooks]}].
 
 all() ->
 	[{group, config_handler_group}].
@@ -33,17 +33,24 @@ test_sync_start(_Config) ->
     application:set_env(elector, sync_start, true),
 	config_handler:sync_start() == true.
 
-test_pre_election_hooks_data(_Config) ->
-	application:set_env(elector, pre_election_hooks, test_hook()),
-	config_handler:pre_election_hooks() == test_hook().
+test_pre_election_hooks(_Config) ->
+	application:set_env(elector, pre_election_hooks, test_hook(pre)),
+	
+	Cond1 = config_handler:pre_election_hooks() == test_hook(pre),
 
-test_post_election_hooks_data(_Config) ->
-	application:set_env(elector, post_election_hooks, test_hook()),
-	config_handler:post_election_hooks() == test_hook().
+test_post_election_hooks(_Config) ->
+	application:set_env(elector, post_election_hooks, test_hook(post)),
+	config_handler:post_election_hooks() == test_hook(post).
 
-test_hook_trigger(pid) ->
-	erlang:send(pid, hook_triggered).
+test_hook_trigger(Type) ->
+	erlang:send(self(), trigger_type(Type)).
 
-test_hook() ->
-	[{?MODULE, test_hook_trigger, [self()]}].
+test_hook(Type) ->
+	[{?MODULE, trigger_type(Type), [self()]}].
+
+trigger_type(Type) ->
+	case Type of
+		pre -> test_pre_hook_trigger;
+		post -> test_post_hook_trigger
+	end.
 	
