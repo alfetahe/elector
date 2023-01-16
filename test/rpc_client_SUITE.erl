4a1f86172ac3385c71780af87888d632b4de6124
Anuar Alfetahe <34630876+alfetahe@users.noreply.github.com>
2023-01-16 09:55:37 +0200

    Update rpc_client_SUITE.erl

--- test/rpc_client_SUITE.erl
+++ test/rpc_client_SUITE.erl
@@ -1,10 +1,10 @@
 -module(rpc_client_SUITE).
 -include_lib("eunit/include/eunit.hrl").
 -export([groups/0, all/0, init_per_group/2, end_per_group/2]).
--export([]).
+-export([test_call/1, test_connected_nodes/1]).
 
 groups() ->
-	[{rpc_client_group, [], [test_rpc_request]}].
+	[{rpc_client_group, [], [test_call, test_connected_nodes]}].
 
 all() ->
 	[{group, rpc_client_group}].
@@ -14,4 +14,10 @@ init_per_group(_GroupName, Config) ->
 	Config.
 
 end_per_group(_GroupName, _Config) ->
+	ok.
+
+test_connected_nodes(_Config) ->
+	ok.
+
+test_call(_Config) ->
 	ok.
