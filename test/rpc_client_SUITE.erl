3f2840afe7d63e9710791790881e12c418f39865
Anuar Alfetahe <34630876+alfetahe@users.noreply.github.com>
2023-01-16 09:50:55 +0200

    Update rpc_client_SUITE.erl

--- test/rpc_client_SUITE.erl
+++ test/rpc_client_SUITE.erl
@@ -1,1 +1,17 @@
 -module(rpc_client_SUITE).
+-include_lib("eunit/include/eunit.hrl").
+-export([groups/0, all/0, init_per_group/2, end_per_group/2]).
+-export([]).
+
+groups() ->
+	[{rpc_client_group, [], [test_rpc_request]}].
+
+all() ->
+	[{group, rpc_client_group}].
+
+init_per_group(_GroupName, Config) ->
+	application:ensure_start(elector),
+	Config.
+
+end_per_group(_GroupName, _Config) ->
+	ok.
