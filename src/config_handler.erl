3a64e810f692c4c01badf62cabdeb281aaf5cb79
Anuar Alfetahe <anuar.alfetahe@gmail.com>
2023-01-28 21:21:55 +0200

    Fixed comments.

--- src/config_handler.erl
+++ src/config_handler.erl
@@ -16,35 +16,35 @@
 %% Exported functions
 %%--------------------------------------------------------------------
 %% @doc Returns the configured election delay.
-%% Default value is `1000` (1 second).
+%% Default value is `1000' (1 second).
 %% @end
 -spec election_delay() -> Delay :: integer().
 election_delay() ->
     application:get_env(elector, election_delay, 1000).
 
 %% @doc Returns the configured strategy module.
-%% Default value is `runtime_high_strategy` (Node with the highest runtime).
+%% Default value is `runtime_high_strategy' (Node with the highest runtime).
 %% @end
 -spec strategy_module() -> StrategyModule :: module().
 strategy_module() ->
     application:get_env(elector, strategy_module, runtime_high_strategy).
 
 %% @doc Returns the configured sync_start flag.
-%% Default value is `true`.
+%% Default value is `true'.
 %% @end
 -spec sync_start() -> SyncStart :: boolean().
 sync_start() ->
     application:get_env(elector, sync_start, true).
 
 %% @doc Returns the configured pre election hooks.
-%% Default value is `[]`.
+%% Default value is `[]'.
 %% @end
 -spec pre_election_hooks() -> Hooks :: [{module(), function(), Args :: list()}].
 pre_election_hooks() ->
     application:get_env(elector, pre_election_hooks, []).
 
 %% @doc Returns the configured post election hooks.
-%% Default value is `[]`.
+%% Default value is `[]'.
 %% @end
 -spec post_election_hooks() -> Hooks :: [{module(), function(), Args :: list()}].
 post_election_hooks() ->
@@ -53,7 +53,7 @@ post_election_hooks() ->
 %% @doc Returns the configured startup hooks enabled flag.
 %% When turned off the post or pre election hooks will not 
 %% be executed on the startup.
-%% Default value is `true`.
+%% Default value is `true'.
 %% @end
 startup_hooks_enabled() ->
     application:get_env(elector, startup_hooks_enabled, true).    
