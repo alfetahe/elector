6812f480e6de220d4eb2e703b819e4fc02f941a9
Anuar Alfetahe <anuar.alfetahe@gmail.com>
2023-07-18 23:38:33 +0300

    Formatted code.

--- test/elector_rt_low_strategy_SUITE.erl
+++ test/elector_rt_low_strategy_SUITE.erl
@@ -11,11 +11,12 @@ all() ->
     [test_elect].
 
 test_elect(_Config) ->
-    {ok, Peer, PeerNode} = ?CT_PEER(["-pa", code:lib_dir(elector) ++ "/ebin", "-connect_all", "false"]),
-        NodeSetupFun =
+    {ok, Peer, PeerNode} =
+        ?CT_PEER(["-pa", code:lib_dir(elector) ++ "/ebin", "-connect_all", "false"]),
+    NodeSetupFun =
         fun() ->
-            application:set_env(elector, automatic_elections, false),
-            application:ensure_started(elector)
+           application:set_env(elector, automatic_elections, false),
+           application:ensure_started(elector)
         end,
     [erpc:call(Node, NodeSetupFun) || Node <- [node(), PeerNode]],
 
